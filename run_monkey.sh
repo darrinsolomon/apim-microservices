ee654fd74b4f8d2f731e3123ad5d96ed849cfd7a
darrinsolomon <darrin.solomon@gmail.com>
2016-08-10 16:21:44 -0400

    autodeploy policy

--- run_monkey.sh
+++ run_monkey.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export SSG_LICENSE="$(cat $HOME/CATechnologies_8011955308725460639_SSG_Gateway_9.xml | gzip | base64)"
 mvn clean install
 docker run \
@@ -22,7 +23,18 @@ docker run \
         caliveapicreator/release
 cd docker
 docker-compose up -d
+sleep 10
+
 cd ..
-read -p "Press [Enter] key to deploy policy..."
+ssg=$(docker logs $(docker ps -q --filter "ancestor=caapim/gateway") 2>&1 | grep "apim-provisioning: INFO: waiting for the shutdown file")
+
+echo "Looking to source control to deploy apim policy; waiting for APIM Gateway to start"
+while [ "$ssg" != "apim-provisioning: INFO: waiting for the shutdown file at \"/userdata/shutdown\" to be created" ]; do
+    echo "waiting for APIM Gateway to start"    
+    ssg=$(docker logs $(docker ps -q --filter "ancestor=caapim/gateway") 2>&1 | grep "apim-provisioning: INFO: waiting for the shutdown file")
+    sleep 10
+done
+
+#read -p "Press [Enter] key to deploy policy..."
 cd ${PWD}/GMU
 ./GatewayMigrationUtility.sh migrateIn -h ms.apim.ca -p 8443 -u pmadmin --plaintextPassword 7layer --trustCertificate --trustHostname --bundle BUNDLES/bundle_ms.xml --results OUT/results.xml
