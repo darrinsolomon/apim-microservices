2f41128774962257c682aa7c3942712038dd5a38
darrinsolomon <darrin.solomon@gmail.com>
2016-09-30 09:53:40 -0400

    depends mod

--- installers/dependencies.sh
+++ installers/dependencies.sh
@@ -4,8 +4,6 @@ sudo service ssh restart
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get -y update
 echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-#echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-#echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer
 sudo apt-get -y install maven
 sudo apt-get -y update
@@ -25,7 +23,7 @@ sudo touch /root/license.xml
 sudo git clone https://github.com/darrinsolomon/apim-microservices.git /root/projects/apim-microservices-master
 sudo unzip -o /root/projects/apim-microservices-master/GMU/GatewayMigrationUtility-1.3.00.zip -d /root/projects/apim-microservices-master/GMU/
 x=$(ifconfig eth0 | perl -nle 's/dr:(\S+)/print $1/ei')
-sudo grep -rl '172.16.160.129' /root/projects --exclude-dir=lacadmin | xargs sed -i s/172.16.160.129/$x/g
+sudo grep -rl '172.16.160.129' /root/projects --exclude-dir=lacadmin | sudo xargs sed -i s/172.16.160.129/$x/g
 echo ----------------------------------------------------
 echo                        
 echo		You must put your ssg license into:
